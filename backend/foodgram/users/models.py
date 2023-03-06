fe8437a639804e43006ed994a781b7c762624be7
dimalright <yagam3333@gmail.com>
2023-03-06 21:01:48 +0300

    pep8 redact

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -15,11 +15,13 @@ class User(AbstractUser):
         (ADMIN, "admin"),
     )
     username_validator = UnicodeUsernameValidator()
-    username = models.CharField(max_length=150, unique=True, validators=[username_validator], verbose_name="Логин")
+    username = models.CharField(max_length=150, unique=True, validators=[
+                                username_validator], verbose_name="Логин")
     email = models.EmailField(unique=True, verbose_name="E-mail")
     first_name = models.CharField(max_length=150, verbose_name="Имя")
     last_name = models.CharField(max_length=150, verbose_name="Фамилия")
-    role = models.CharField(max_length=30, choices=ROLES, default="user", verbose_name="Роль")
+    role = models.CharField(max_length=30, choices=ROLES,
+                            default="user", verbose_name="Роль")
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
@@ -27,7 +29,8 @@ class User(AbstractUser):
         ordering = ["id"]
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
-        constraints = [models.CheckConstraint(check=~models.Q(username__iexact="me"), name="username_is_not_me")]
+        constraints = [models.CheckConstraint(check=~models.Q(
+            username__iexact="me"), name="username_is_not_me")]
 
     def __str__(self):
         return self.username
@@ -44,11 +47,13 @@ class Follow(models.Model):
         related_name="follower",
         verbose_name="Подписчик",
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following", verbose_name="Блогер")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following", verbose_name="Блогер")
 
     class Meta:
         verbose_name = "Подписка"
         verbose_name_plural = "Подписки"
         constraints = [
-            models.UniqueConstraint(fields=["user", "author"], name="unique_follow"),
+            models.UniqueConstraint(
+                fields=["user", "author"], name="unique_follow"),
         ]
