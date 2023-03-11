c71083535fc7978dd58af06714a88ece36a6c9e6
dimalright <yagam3333@gmail.com>
2023-03-11 14:05:13 +0300

    more changes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -111,7 +111,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
